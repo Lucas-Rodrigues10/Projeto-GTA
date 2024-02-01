22638d66bb7e2f86c0ffe5700dd8495a93264e29
Lucas Rodrigues <lucasrodriguesy15@gmail.com>
2024-02-01 16:18:07 -0300

    Refatorando o código JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,18 +1,7 @@
-// Passo 1 - Pegar o botão de seleção de plataformas no JS para verificar quando o usuário clicar em cima dele 
 const botao = document.querySelector('.btn-plataforma');
 
-//Passo 2 - Pegar o elemento do conteudo que precisa ser mostrado 
 const elementoPlatafomas = document.querySelector('.btn-plataforma .plataformas');
 
-//Passo 3 - Pegar o clique do usuário no JS
 botao.addEventListener("click", () => {
-    const botaoEstaAberto = (elementoPlatafomas.classList.contains("ativo"))
-    //Passo 4 - Verificar se o conetudo do botão já esta aberto, se sim, devemos remover a classe ativo para que ele esconda o conteúddo novamente
-    if(botaoEstaAberto){
-        elementoPlatafomas.classList.remove("ativo");
-    }
-     //Passo 5 - Quando o usuário clicar, adicionar a classe ativo na listagem de plataformas dentro do botão para que o conteúdo apareça
-    else{
-        elementoPlatafomas.classList.add("ativo")
-    }
+        elementoPlatafomas.classList.toggle("ativo");
 })
